c7241c2871ebbf0b34ecee8bb75f5f56ffd0b395
mariusgitar <161877670+mariusgitar@users.noreply.github.com>
2026-05-26 22:46:58 +0200

    Add homepage create workshop button flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import CreateButton from '../components/CreateButton'
+
 export default function Page() {
   return (
     <main>
       <h1>Workshop Agenda</h1>
+      <p>Planlegg workshopen din</p>
+      <CreateButton />
     </main>
-  );
+  )
 }
